848eba5b5956530838932916ce55321a801f1d9c
Femi <fagbemibabs18@gmail.com>
2026-06-23 14:34:06 +0100

    renamed base page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text className="text-red-500">Edit src/app/index.tsx to edit this screen.</Text>
+      <Text className="text-yellow-500">Edit src/app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
